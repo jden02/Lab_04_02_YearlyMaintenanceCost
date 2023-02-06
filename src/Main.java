e3f79986b1f5eac68ac04b8a57661005f3508f83
jden02 <jden71002@gmail.com>
2023-02-05 19:51:04 -0500

    Task 2 completed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        double mCostWinter = 1200.00;
+        double mCostSpring = 800.00;
+        double mCostSummer = 650.00;
+        double mCostFall = 999.99;
 
-        
+        double mCostTotal = mCostFall + mCostSpring + mCostSummer + mCostWinter;
+
+        System.out.println("The maintenance costs for WINTER = " + mCostWinter);
+        System.out.println("The maintenance costs for SPRING = " + mCostSpring);
+        System.out.println("The maintenance costs for SUMMER = " + mCostSummer);
+        System.out.println("The maintenance costs for FALL = " + mCostFall);
+        System.out.println("The total maintenance cost = " + mCostTotal);
     }
 }
